2aa455e9b0402babdb1baac4a82f0946cf841722
Nguyễn Tiến Thắng <nguyentjenthang2003@gmail.com>
2026-06-22 10:14:26 +0700

    update

---
+++ junit-calculator/junit_calcultor/src/SimpleCalculator.java
@@ -0,0 +1,10 @@
+public class SimpleCalculator {
+
+    public static int add(int first, int second) {
+        throw new UnsupportedOperationException("Chưa cài đặt phương thức add()");
+    }
+
+    public static int sub(int first, int second) {
+        throw new UnsupportedOperationException("Chưa cài đặt phương thức sub()");
+    }
+}
